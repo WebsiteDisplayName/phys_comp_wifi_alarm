6766e9bea99796fa67a25f436bfaabdc56598daa
WebsiteDisplayName <rowemg@bc.edu>
2023-05-01 22:43:41 -0400

    more obfuscation

--- key_conv.py
+++ key_conv.py
@@ -8,8 +8,8 @@ def basic_auth(username, password):
     return f'Basic {token}'
 
 
-public_key = "0dee252d678a0a0c1440bc937af60817"
-private_key = "4b720062884d73d3da9297c21deb75d6"
+public_key = "public key numbers"
+private_key = "private key numbers"
 
 out_val = basic_auth(public_key, private_key)
 print(out_val)
